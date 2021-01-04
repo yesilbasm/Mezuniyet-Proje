94e6b70c933117b1390680a4635d780acfd3db48
Mustafa Zübeyr Yeşilbaş <mustafazubeyryesilbas@gmail.com>
2021-01-04 05:11:04 +0300

    LevelDesign

--- Assets/[Game]/GameProject - Rename this/Scripts/ObstacleController.cs
+++ Assets/[Game]/GameProject - Rename this/Scripts/ObstacleController.cs
@@ -1,9 +1,73 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Diagnostics;
 using System;
 using Sirenix.Utilities;
 using Debug = UnityEngine.Debug;
+using DG.Tweening;
     public class ObstacleController : MonoBehaviour
     {
         
+        public float RotateSpeed;
+        public bool CanRotate;
+        public float MoveDistance;
+        public float MoveTime;
+        public float MoveDistanceBack;
+        public float MoveDistanceForward;
+
+        public bool CanMoveRight ;
+        public bool CanMoveLeft;
+
+        public bool CanMoveForward;
+        public bool CanMoveBack;
+
+        private void FixedUpdate() 
+        {
+            ObstacleMovement();
+        }
+
+        private void ObstacleMovement()
+        {
+            if(CanRotate)
+            {
+               // transform.RotateAround(transform.position , Vector3.up , 360 * Time.fixedDeltaTime );
+                transform.RotateAround(transform.position, Vector3.up, 1 * Time.fixedDeltaTime * RotateSpeed);            }
+
+            if(CanMoveRight )
+            {
+                transform.DOMoveX( MoveDistance , MoveTime).OnComplete( () => CanMoveLeft=true );
+                CanMoveRight = false;
+                
+
+            }
+
+            if(CanMoveLeft)
+            {
+                transform.DOMoveX(-MoveDistance , MoveTime ).OnComplete( () => CanMoveRight=true);
+                CanMoveLeft = false ;
+                
+            }
+
+            if(CanMoveForward )
+            {
+                transform.DOMoveZ( MoveDistanceBack , MoveTime).OnComplete( () => CanMoveBack=true );
+                CanMoveForward = false;
+                
+
+            }
+
+            if(CanMoveBack )
+            {
+                transform.DOMoveZ( MoveDistanceForward , MoveTime).OnComplete( () => CanMoveForward=true );
+                CanMoveBack = false;
+                
+
+            }
+
+
+
+        }
+
+        
+
+
     }
